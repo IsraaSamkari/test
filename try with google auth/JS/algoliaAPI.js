0a32b1cc982b45c27979fc722d9d34cd53ab815f
israasamkari <israasamkari@hotmail.com>
2022-04-01 16:07:36 +0300

    delete file not to need in auth branch

--- try with google auth/JS/algoliaAPI.js
+++
@@ -1,9 +0,0 @@
-const app = document.getElementById("app");
-				const client = algoliasearch('HA5QLSQEEG', '6ba3d86a4df6ee42eccf44abe2ef5720');
-				const index = client.initIndex('test_Dictionary');
-				
-				index.search("").then(result =>{
-					const hits = result.hits.map( hit => `<p>${hit.firstname}</p>`);
-					app.innerHTML = hits.join("");
-				
-				});
