83f9b49c61fc75d3054dbb5f5a33e3d5359f016f
israasamkari <israasamkari@hotmail.com>
2022-04-01 18:32:28 +0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -25,12 +25,12 @@ signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
-	console.log(user);
+	console.log("has email");
     // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-	console.log(errorCode);
-    console.log(errorMessage);
+	console.log("error code");
+    console.log("errorMessage");
   });
